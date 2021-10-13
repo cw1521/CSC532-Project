342509c06ebc54184b0bc764ae199d8092b7fc3f
christian williams <cw1521@jagmail.southalabama.edu>
2021-10-13 07:51:45 -0500

    initial commit

--- src/main.py
+++ src/main.py
@@ -7,14 +7,18 @@ import sys
 # From command line type python main.py c to start client
 # From command line type python main.py s to start server
 # From command line type python main.py cs to start clientserver
+def main(app):
+    if app == "s":
+        server.start()
+    elif app == "c":
+        client.start()
+    elif app == "cs":
+        clientserver.start()
+
 
 if __name__ == "__main__":
 
     args = sys.argv
+    app = args[1]
 
-    if args[1] == "s":
-        server.start()
-    elif args[1] == "c":
-        client.start()
-    elif args[1] == "cs":
-        clientserver.start()
+    main(app)
