eec479b71bb46ed85d213ec5090c0fde68cc0650
christian williams <cw1521@jagmail.southalabama.edu>
2021-10-13 21:15:49 -0500

    initial calculator commit

---
+++ calculator/src/server/calculate/calculate.py
@@ -0,0 +1,41 @@
+import re
+
+
+def getValidationRegex():
+    white_list = '''acos|asin|atan|atan2|ceil|cos|cosh|
+        degrees|exp|fabs|floor|fmod|frexp|hypot|ldexp|
+        log|log10|modf|pow|radians|sin|sinh|sqrt|tan|tanh'''
+    regexString = '[^' + white_list + '|0-9|+\-\*/\^\(\)]'
+    return re.compile(regexString)
+
+
+
+def isValidInput(funcString):
+    regex = getValidationRegex()
+    match = regex.search(funcString)
+    if match == None:
+        return True
+    else:
+        return False
+
+
+
+def calculate(funcString):
+    if isValidInput(funcString):
+        funcString = funcString.replace("^", "**")
+        try:
+            answer = eval(funcString)
+            return answer
+        except:
+            return "error"
+    else:
+        return "error"
+
+
+
+def calculateList(funcList):
+    newList = []
+    for i in range(len(funcList)):
+        funcString = funcList[i]
+        newList.append((funcString, calculate(funcString)))
+    return newList
--- calculator/src/server/server.py
+++ calculator/src/server/server.py
@@ -1,3 +1,14 @@
+from src.server.calculate import calculate as calc
+
 
 def start():
+
     print("Server is running.")
+
+
+    # TESTING
+
+    funcList = ["5*4", "5^4", "(5+6)", "(5*(4+ 3))", "run test"]    
+    newList = calc.calculateList(funcList)
+
+    print(newList)
