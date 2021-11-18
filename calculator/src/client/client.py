de3b40ede04dc71e0ecbfd657b9641d3fa648bbe
christian williams <cw1521@jagmail.southalabama.edu>
2021-11-18 13:51:08 -0600

    commited

--- calculator/src/client/client.py
+++ calculator/src/client/client.py
@@ -38,6 +38,86 @@ import socket as sock
  
 
 
+from src.calculator import calculator as calc
+from src.sockethandler import socket_handler as sh
+import socket as sock
+from src.jsonhandler import json_handler as json
+
+
+
+
+# def handle_connection(conn, addr):
+#     # Receive data
+
+#     # Convert received data from json string to list
+
+#     # Calculate the answers from the list
+
+#     # Convert the answer list to a json string
+
+#     # Send the data over the socket as a UTF-8 encoded string
+
+
+
+
+# def start(host, port):
+#     # Open socket
+
+#         # Bind to host and port
+
+#         # Listen for incoming connections
+     
+#         # While true
+
+#             # Accept incoming connections
+
+#             # Handle the incoming connections
+
+#             # Close the connection
+
+
+from src.json import json_handler as json
+import socket as sock
+from os import getcwd as getcwd
+from src.sockethandler import socket_handler as sh
+
+
+
+
+
+def read_file(file_path):
+    file_list = []
+    with open(file_path, 'r') as f:
+        for line in f.readlines():
+            file_list.append(line.strip())
+    return file_list
+
+
+def shutdown_server(socket):
+    sh.send_data(socket, 'shutdown'.encode())
+
+
+def start(host, port):
+    print("Client is running.")
+    file_path = 'test/src/functions.txt'
+    file_list = read_file(file_path)
+    # print(file_list)
+
+    file_json = json.get_json_from_list(file_list)
+    # print(file_json)
+
+    message = ''
+    # with sock.socket(sock.AF_INET, sock.SOCK_STREAM) as s:
+    #     s.connect((host, port))
+    #     sh.send_data(s, file_json.encode())
+    #     s.shutdown(sock.SHUT_WR)
+    #     message = sh.receive_data(s)
+    # print('message received: ', message)
+
+
+    with sock.socket(sock.AF_INET, sock.SOCK_STREAM) as s:
+        s.connect((host, port))
+        shutdown_server(s)
 
 
 
