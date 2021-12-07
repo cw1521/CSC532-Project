971a2bd7e1d7005527d01e90fadae1fe8d4b0553
christian williams <cw1521@jagmail.southalabama.edu>
2021-12-07 10:41:20 -0600

    fixed comments in server.py

--- calculator/src/server/server.py
+++ calculator/src/server/server.py
@@ -4,51 +4,25 @@ import socket as sock
 from src.jsonhandler import json_handler as json
 
 
-
-
-# def handle_connection(conn, addr):
-#     # Receive data
-
-#     # Convert received data from json string to list
-
-#     # Calculate the answers from the list
-
-#     # Convert the answer list to a json string
-
-#     # Send the data over the socket as a UTF-8 encoded string
-
-
-
-
-# def start(host, port):
-#     # Open socket
-
-#         # Bind to host and port
-
-#         # Listen for incoming connections
-     
-#         # While true
-
-#             # Accept incoming connections
-
-#             # Handle the incoming connections
-
-#             # Close the connection
 from src.calculator import calculator as calc
 import socket as sock
 from src.jsonhandler import json_handler as json
 from src.sockethandler import socket_handler as sh
 
 
-
 def handle_connection(conn, addr):
+    # Receive data
     message = sh.receive_data(conn)
     print('Received: ', message)
     if message != 'shutdown':
+        # Convert received data from json string to list
         func_list = json.get_list_from_json(message)
+        # Calculate the answers from the list
         answer = calc.calculate_list(func_list)
+        # Convert the answer list to a json string
         answer_json = json.get_json_from_list(answer)
         print('Sending: ', answer_json)
+        # Send the data over the socket as a UTF-8 encoded string
         sh.send_data(conn, answer_json.encode())
     else:
         return 'shutdown'
@@ -56,12 +30,20 @@ def handle_connection(conn, addr):
 
 
 def start(host, port):
+     # Open socket
     with sock.socket(sock.AF_INET, sock.SOCK_STREAM) as s:
+        # Bind to host and port
         s.bind((host, port))
+        # Listen for incoming connections
         s.listen()
+
         while True:
+            # Accept incoming connections
             conn, addr = s.accept()
+            # Handle the incoming connections
             command = handle_connection(conn, addr)
+            
+            # Close the connection
             conn.close()
             if command == 'shutdown':
                 print("shutting down server")
